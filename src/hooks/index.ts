3b6227390edeeb20ebdf097b8c8514b9d666bdb3
pejosecalderont <cloudencuestasinternas2021@hotmail.com>
2022-09-03 11:27:15 -0500

    v5

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,9 +2,7 @@ import { EventosRepo } from '$lib/repos';
 import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
-	event.locals.eventosRepo = new EventosRepo(
-		'AccountEndpoint=https://encuestasdb.documents.azure.com:443/;AccountKey=jbUfTnFcRnprj1rKnuMxQXBsiJr4ph2MTxdyDWhKYHNZe0z0Et6ecKIkKnVOcY8uaWBTiPDG3BjBetDtj7lfpA=='
-	);
+	event.locals.eventosRepo = new EventosRepo(import.meta.env.VITE_DATABASE_URL);
 
 	const response = await resolve(event);
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,5 +3,5 @@
 	console.log('data', data);
 </script>
 
-<h1>Welcome to SvelteKit demo 3 {data.eventos.length}</h1>
+<h1>Welcome to SvelteKit demo 4 {data.eventos.length}</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
