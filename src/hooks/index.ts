f4819db29fa20cf39c76ed0b0984796fac702309
pejosecalderont <cloudencuestasinternas2021@hotmail.com>
2022-09-03 09:47:11 -0500

    fix repo dec

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,4 +1,5 @@
 import type { Handle } from '@sveltejs/kit';
+import { EventosRepo } from '../lib/repos';
 
 export const handle: Handle = async ({ event, resolve }) => {
 	event.locals.eventosRepo = new EventosRepo(import.meta.env.VITE_DATABASE_URL);
