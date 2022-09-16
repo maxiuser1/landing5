1d72ece95059d4caf738f0ce5515df369b867881
jose.calderon <jose.calderon@tranzact.net>
2022-09-16 15:59:56 -0500

    header ul

---
+++ src/lib/components/Layout/Header/Header.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let categories: Array<string>;
+</script>
+
+<header class="main">
+	<nav class="container">
+		<ul>
+			{#each categories as category}
+				<li>{category}</li>
+			{/each}
+		</ul>
+	</nav>
+</header>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,15 @@
-<nav>
-	<a href="/">Home</a>
-	<a href="/about">About</a>
-	<a href="/settings">Settings</a>
-</nav>
+<script lang="ts">
+	import { Header } from '$lib/components/Layout';
+	let categories: Array<string> = [
+		'Conciertos',
+		'Teatro',
+		'Deportes',
+		'Festrivales',
+		'Arte y Cultura',
+		'Niños'
+	];
+</script>
+
+<Header {categories} />
 
 <slot />
