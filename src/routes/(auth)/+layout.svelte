015734fed0c9467abe68eecc493330b6150893d1
jose.calderon <jose.calderon@tranzact.net>
2022-09-27 21:10:21 -0500

    master pages

---
+++ src/routes/(auth)/+layout.svelte
@@ -0,0 +1,15 @@
+<svelte:head>
+	<style>
+		body {
+			margin-top: 0 !important;
+			/* background: linear-gradient(180deg, #ff0030 0%, #ff00ad 53.12%, #9023fe 100%); */
+		}
+	</style>
+</svelte:head>
+
+<div>
+	<div>hola</div>
+	<div>
+		<slot />
+	</div>
+</div>
