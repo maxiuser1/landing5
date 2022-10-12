8a1517204b97e21a1e33b2ff22b8823753b5fc94
jose.calderon <jose.calderon@tranzact.net>
2022-10-12 12:06:40 -0500

    agrega chat

--- src/routes/(qh)/+layout.svelte
+++ src/routes/(qh)/+layout.svelte
@@ -11,6 +11,15 @@
 	];
 </script>
 
+<svelte:head>
+	<!-- Start of quehay Zendesk Widget script -->
+	<script
+		id="ze-snippet"
+		src="https://static.zdassets.com/ekr/snippet.js?key=0b8464e9-9d32-4b3a-945a-02f19d295df7">
+	</script>
+	<!-- End of quehay Zendesk Widget script -->
+</svelte:head>
+
 <Header {categories} />
 <main>
 	<slot />
