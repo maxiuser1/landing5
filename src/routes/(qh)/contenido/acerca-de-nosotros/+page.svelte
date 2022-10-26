548a108351e9c5f609c8e5404eda4e172dc969fa
jose.calderon <jose.calderon@tranzact.net>
2022-10-26 10:59:52 -0500

    acerca de nosotros 2

--- src/routes/(qh)/contenido/acerca-de-nosotros/+page.svelte
+++ src/routes/(qh)/contenido/acerca-de-nosotros/+page.svelte
@@ -38,6 +38,31 @@
 	</div>
 </section>
 
+<section class="breacrumbs">
+	<div class="container">
+		<ul>
+			<li>
+				<a href="/">
+					<svg
+						width="16"
+						height="16"
+						viewBox="0 0 16 16"
+						fill="none"
+						xmlns="http://www.w3.org/2000/svg"
+					>
+						<path d="M16 7H3.83L9.42 1.41L8 0L0 8L8 16L9.41 14.59L3.83 9H16V7Z" fill="#D30ED1" />
+					</svg>
+				</a>
+			</li>
+			<li class="selected">
+				<a href="/"> Inicio </a>
+			</li>
+			<li>></li>
+			<li>Acerca de nosotros</li>
+		</ul>
+	</div>
+</section>
+
 <section class="container">
 	<div class="info ">
 		<div>
@@ -78,6 +103,26 @@
 </section>
 
 <style lang="scss">
+	.breacrumbs {
+		background: white;
+		ul {
+			padding: 24px 0px;
+			display: flex;
+			gap: 14px;
+			li {
+				font-weight: 500;
+				font-size: 16px;
+				line-height: 24px;
+				&.selected {
+					a {
+						font-weight: 800;
+						color: #d30ed1;
+					}
+				}
+			}
+		}
+	}
+
 	.info {
 		display: flex;
 		justify-content: space-between;
