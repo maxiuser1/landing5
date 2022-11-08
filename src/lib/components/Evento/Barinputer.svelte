c09d5202b58cfc561c8227009c9f3d81c2efdf4c
jose.calderon <jose.calderon@tranzact.net>
2022-11-08 17:04:46 -0500

    caso revertido

--- src/lib/components/Evento/Barinputer.svelte
+++ src/lib/components/Evento/Barinputer.svelte
@@ -1,13 +1,10 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
 	import { Html5Qrcode } from 'html5-qrcode';
-
-	export let value: string | null | undefined = null;
-	export let name: string | null | undefined = null;
-
-	let html5Qrcode: any;
+	import { onMount } from 'svelte';
 
 	let scanning = false;
+
+	let html5Qrcode: any;
 
 	onMount(init);
 
@@ -15,15 +12,12 @@
 		html5Qrcode = new Html5Qrcode('reader');
 	}
 
-	let dialog: any;
-
 	function start() {
-		console.log('start');
 		html5Qrcode.start(
 			{ facingMode: 'environment' },
 			{
 				fps: 10,
-				qrbox: { width: 380, height: 380 }
+				qrbox: { width: 250, height: 250 }
 			},
 			onScanSuccess,
 			onScanFailure
@@ -32,70 +26,30 @@
 	}
 
 	async function stop() {
-		console.log('stop');
-		dialog.close();
 		await html5Qrcode.stop();
 		scanning = false;
 	}
 
 	function onScanSuccess(decodedText, decodedResult) {
-		value = decodedText;
+		alert(`Code matched = ${decodedText}`);
+		console.log(decodedResult);
 	}
 
 	function onScanFailure(error) {
 		console.warn(`Code scan error = ${error}`);
 	}
-
-	const showDialogClick = () => {
-		dialog.showModal();
-	};
 </script>
 
-<input type="text" bind:value {name} on:change on:blur />
-<button on:click={() => showDialogClick()} type="button">Show dialog as modal</button>
+<main>
+	<reader id="reader" />
+	{#if scanning}
+		<button on:click={stop}>stop</button>
+	{:else}
+		<button on:click={start}>start</button>
+	{/if}
+</main>
 
-<dialog bind:this={dialog} class="modal" id="modal">
-	<main class="content-modal">
-		<reader id="reader" />
-		{#if scanning}
-			<button on:click={stop} class="btn" type="button">Parar</button>
-		{:else}
-			<button on:click={start} class="btn" type="button">Scan</button>
-		{/if}
-	</main>
-</dialog>
-
-<style lang="scss">
-	.modal {
-		min-width: 100%;
-		background: white;
-		border: none;
-		margin: 0 auto;
-		margin-top: 24px;
-
-		input {
-			min-width: 250px;
-			max-width: 100%;
-			box-sizing: border-box;
-			padding: 0 12px;
-			border: 1px solid #c6c6c6;
-			border-radius: 4px;
-			background: #ffffff;
-			height: 42px;
-		}
-
-		.form-group {
-			margin-bottom: 32px;
-		}
-
-		.btn {
-			width: 100%;
-			font-weight: 600;
-			font-size: 16px;
-			line-height: 24px;
-		}
-	}
-
+<style>
 	main {
 		display: flex;
 		flex-direction: column;
@@ -106,7 +60,7 @@
 
 	reader {
 		width: 100%;
-		min-height: 70vh;
+		min-height: 500px;
 		background-color: black;
 	}
 </style>
