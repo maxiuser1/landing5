bc9467d1322519ec0e87c51010bf7bf3ef3690f9
josec <pe.jose.calderon@gmail.com>
2022-11-10 21:20:00 -0500

    porfavor

--- src/routes/(qh)/[slug]/venta/+page.svelte
+++ src/routes/(qh)/[slug]/venta/+page.svelte
@@ -210,7 +210,14 @@
 				halfSample: true,
 				patchSize: "medium",
 			},
-		});
+		}, function(err) {
+      if (err) {
+          console.log(err);
+          return
+      }
+      console.log("Initialization finished. Ready to start");
+      Quagga.start();
+  });
 
 		Quagga.onDetected((t: any) => {
 
