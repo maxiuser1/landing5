edf7b8af5ef7a180245d70ecb0edb9006b83fbb3
josec <pe.jose.calderon@gmail.com>
2022-11-10 21:05:03 -0500

    prueba de fuegop

--- src/routes/(qh)/[slug]/venta/+page.svelte
+++ src/routes/(qh)/[slug]/venta/+page.svelte
@@ -178,10 +178,39 @@
     },
 };
 
-		Quagga.config(defaultConfig).fromSource({
-			...defaultConfig.inputStream,
-			target: '#reader'
-		})
+		Quagga.init({
+			frequency: 5,
+			numOfWorkers: 2,
+			locate: true,
+			inputStream: {
+				name: "Live",
+				type: "LiveStream",
+				target:   '#reader',
+				constraints: {
+					width: 800,
+					height: 600,
+					deviceId: 0,
+					facingMode: "environment",
+				},
+				area: {
+					top: "0%",
+					right: "0%",
+					left: "0%",
+					bottom: "0%",
+				},
+			},
+			decoder: {
+				readers: [
+					'ean_reader',
+					'code_39_reader',
+					'code_128_reader',
+				],
+			},
+			locator: {
+				halfSample: true,
+				patchSize: "medium",
+			},
+		});
 
 		Quagga.onDetected((t: any) => {
 
