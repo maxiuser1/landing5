9c9ba49f098a79de7ea0738b32eb33ab3d4ce591
jose.calderon <jose.calderon@tranzact.net>
2022-11-15 12:42:32 -0500

    arreglo box

--- src/lib/icons/Box.svelte
+++ src/lib/icons/Box.svelte
@@ -35,28 +35,34 @@
 			d="M1 0 3 0A1 1 90 013 2L1 2A1 1 90 011 0ZM6 0 8 0A1 1 90 018 2L6 2A1 1 90 016 0ZM11 0 13 0A1 1 90 0113 2L11 2A1 1 90 0111 0ZM3 3 11 3A1 1 90 0111 9L3 9A1 1 90 013 3ZM1 10 3 10A1 1 90 013 12L1 12A1 1 90 011 10ZM6 10 8 10A1 1 90 018 12L6 12A1 1 90 016 10ZM11 10 13 10A1 1 90 0113 12L11 12A1 1 90 0111 10Z"
 		/>
 	</svg> -->
-
-	<svg viewBox="0 0 112 60" class="box" on:click={handleClick} {width} xmlns="http://www.w3.org/2000/svg">
-		<g class="layer">
-			<title />
-			<rect fill="none" height="8" id="svg_2" rx="4" transform="matrix(1 0 0 1 0 0)" width="22.75" x="15.44" y="51" stroke="#D4D4D4" />
-			<rect fill={color} height="8" id="svg_8" rx="4" width="22.75" x="44.75" y="51" />
-			<rect fill={color} height="8" id="svg_9" rx="4" width="22.75" x="73.75" y="51" />
-			<rect fill={color} height="8" id="svg_10" rx="4" transform="matrix(1 0 0 1 0 0)" width="22.75" x="15.44" y="0.75" />
-			<rect fill={color} height="8" id="svg_11" rx="4" transform="matrix(1 0 0 1 0 0)" width="22.75" x="44.75" y="0.63" />
-			<rect fill={color} height="8" id="svg_12" rx="4" width="22.75" x="73.75" y="0.38" />
-			<rect fill={color} height="8" id="svg_13" rx="4" transform="matrix(0 -0.703351 1 0 81.875 91.6459)" width="22.75" x="65.43" y="-79.63" />
-			<rect fill={color} height="8" id="svg_17" rx="4" transform="matrix(0 -0.643713 1 0 81.875 86.642)" width="22.75" x="92.62" y="-79.75" />
-			<rect fill={color} height="8" id="svg_18" rx="4" transform="matrix(0 -0.703351 1 0 81.875 91.6459)" width="22.75" x="65.43" y="20.44" />
-			<rect fill={color} height="8" id="svg_19" rx="4" transform="matrix(0 -0.703351 1 0 81.875 91.6459)" width="22.75" x="89.95" y="20.56" />
-			<rect fill={color} height="36" id="svg_20" rx="4" width="81.62" x="15.44" y="11.63" />
-		</g>
-	</svg>
+	<button on:click={handleClick} type="button" class="box">
+		<svg viewBox="0 0 112 60" {width} xmlns="http://www.w3.org/2000/svg">
+			<g class="layer">
+				<title />
+				<rect fill="none" height="8" id="svg_2" rx="4" transform="matrix(1 0 0 1 0 0)" width="22.75" x="15.44" y="51" stroke="#D4D4D4" />
+				<rect fill={color} height="8" id="svg_8" rx="4" width="22.75" x="44.75" y="51" />
+				<rect fill={color} height="8" id="svg_9" rx="4" width="22.75" x="73.75" y="51" />
+				<rect fill={color} height="8" id="svg_10" rx="4" transform="matrix(1 0 0 1 0 0)" width="22.75" x="15.44" y="0.75" />
+				<rect fill={color} height="8" id="svg_11" rx="4" transform="matrix(1 0 0 1 0 0)" width="22.75" x="44.75" y="0.63" />
+				<rect fill={color} height="8" id="svg_12" rx="4" width="22.75" x="73.75" y="0.38" />
+				<rect fill={color} height="8" id="svg_13" rx="4" transform="matrix(0 -0.703351 1 0 81.875 91.6459)" width="22.75" x="65.43" y="-79.63" />
+				<rect fill={color} height="8" id="svg_17" rx="4" transform="matrix(0 -0.643713 1 0 81.875 86.642)" width="22.75" x="92.62" y="-79.75" />
+				<rect fill={color} height="8" id="svg_18" rx="4" transform="matrix(0 -0.703351 1 0 81.875 91.6459)" width="22.75" x="65.43" y="20.44" />
+				<rect fill={color} height="8" id="svg_19" rx="4" transform="matrix(0 -0.703351 1 0 81.875 91.6459)" width="22.75" x="89.95" y="20.56" />
+				<rect fill={color} height="36" id="svg_20" rx="4" width="81.62" x="15.44" y="11.63" />
+			</g>
+		</svg>
+	</button>
 {/if}
 
 <style>
-	svg.box:hover rect {
+	.box:hover rect {
 		fill: #ff5260;
 		pointer-events: all;
+	}
+
+	.box {
+		background: none;
+		border: none;
 	}
 </style>
