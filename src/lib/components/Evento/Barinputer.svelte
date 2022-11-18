79d1f3d9dbd7f0411cfda0dea66297db10be4a71
josec <pe.jose.calderon@gmail.com>
2022-11-17 20:38:25 -0500

    cambio quagga

--- src/lib/components/Evento/Barinputer.svelte
+++ src/lib/components/Evento/Barinputer.svelte
@@ -1,95 +1,31 @@
 <script lang="ts">
 	import { createEventDispatcher, onDestroy, onMount } from 'svelte';
-	import Quagga from '@ericblade/quagga2';
+	import { BarcodeScanner } from 'dynamsoft-javascript-barcode';
+
 	const dispatch = createEventDispatcher();
-
+	let barcontainer: any;
+	let scanner: any;
 	onMount(async () => {
-		console.log('BAR ONOMUNT');
-		Quagga.init(
-			{
-				frequency: 5,
-				numOfWorkers: 2,
-				locate: true,
-				inputStream: {
-					name: 'Live',
-					type: 'LiveStream',
-					target: '.overlay__content',
-					constraints: {
-						width: 800,
-						height: 600,
-						deviceId: 0,
-						facingMode: 'environment'
-					},
-					area: {
-						top: '0%',
-						right: '0%',
-						left: '0%',
-						bottom: '0%'
-					}
-				},
-				decoder: {
-					readers: ['code_39_reader']
-				},
-				locator: {
-					halfSample: true,
-					patchSize: 'medium'
-				}
-			},
-			function (err) {
-				if (err) {
-					console.log(err);
-					return;
-				}
-				Quagga.start();
-			}
-		);
+		BarcodeScanner.license = 'DLS2eyJoYW5kc2hha2VDb2RlIjoiMTAxNDY5NjA4LVRYbFhaV0pRY205cVgyUmljZyIsIm9yZ2FuaXphdGlvbklEIjoiMTAxNDY5NjA4IiwiY2hlY2tDb2RlIjotMTU5ODI4OTE0MX0=';
+		scanner = await BarcodeScanner.createInstance();
+
+		scanner.onUniqueRead = (txt: any, result: any) => {
+			dispatch('detected', { text: txt });
+		};
+		await scanner.show();
 	});
-
-	Quagga.onDetected(errorCheck);
-
-	function errorCheck(result: any) {
-		dispatch('detected', { text: result?.codeResult?.code });
-		// alert(JSON.stringify(result));
-		// const err = getMedianOfCodeErrors(result.codeResult.decodedCodes);
-		// if (err < 0.25) {
-		// 	const code = result?.codeResult?.code;
-		// 	dispatch('detected', code);
-		// }
-	}
-
-	function getMedianOfCodeErrors(decodedCodes: any) {
-		const errors = decodedCodes.filter((x: any) => x.error !== undefined).map((x) => x.error);
-		const medianOfErrors = getMedian(errors);
-		return medianOfErrors;
-	}
-
-	function getMedian(arr: any) {
-		arr.sort((a: any, b: any) => a - b);
-		const half = Math.floor(arr.length / 2);
-		if (arr.length % 2 === 1) {
-			return arr[half];
-		}
-		return (arr[half - 1] + arr[half]) / 2;
-	}
 
 	onDestroy(() => {
-		console.log('DESTROYED');
-		Quagga.stop();
+		scanner.destroyContext();
 	});
 
-	function stop() {
-		Quagga.stop();
+	async function stop() {
+		await scanner.stop();
 		dispatch('closed', { text: 'aa' });
 	}
 </script>
 
 <button on:click={stop} type="button" class="btn">Cerrar</button>
-<div class="overlay__content" />
+<div bind:this={barcontainer} />
 
-<style>
-	.overlay__content {
-		width: 100%;
-		min-height: 350px;
-		background-color: black;
-	}
 </style>
