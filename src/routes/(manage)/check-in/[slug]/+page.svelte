1e0b447a9715028247a85970b89348e48ade9432
jose.calderon <jose.calderon@tranzact.net>
2023-05-04 13:34:19 -0500

    test

---
+++ src/routes/(manage)/check-in/[slug]/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import QrScanner from '$lib/components/Evento/QrScanner.svelte';
+
+	let scanning = false;
+	let camara = false;
+
+	function scanCanceld(event: any) {}
+	function scanned(event: any) {}
+</script>
+
+<section>
+	<div class="modal" style:visibility={camara ? 'visible' : 'hidden'}>
+		<QrScanner on:detected={scanned} on:closed={scanCanceld} />
+	</div>
+</section>
