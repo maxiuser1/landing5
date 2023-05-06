f6ddd4e85535abaf5016077f41968d0d3badedbf
jose.calderon <jose.calderon@tranzact.net>
2023-05-06 17:25:32 -0500

    ingreso manual

---
+++ src/routes/(manage)/check-in/[slug]/manual/+page.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+	let numeroTicket: any;
+	const clickGeneral = () => {
+		window.location.href = `/check-in/vilmapalma/impreso/GENERAL/${numeroTicket.value}`;
+	};
+
+	const clickVip = () => {
+		window.location.href = `/check-in/vilmapalma/impreso/VIP/${numeroTicket.value}`;
+	};
+
+	const clickSuper = () => {
+		window.location.href = `/check-in/vilmapalma/impreso/SUPER/${numeroTicket.value}`;
+	};
+</script>
+
+<section class="container">
+	<div class="formulario">
+		<div class="form-group">
+			<h4>Picado de ticket físico</h4>
+		</div>
+		<div class="form-group">
+			<input type="text" placeholder="Número de ticket" bind:this={numeroTicket} />
+		</div>
+		<div class="form-group">
+			<button on:click={clickGeneral} class="btn">GENERAL</button>
+		</div>
+
+		<div class="form-group">
+			<button on:click={clickVip} class="btn">VIP</button>
+		</div>
+
+		<div class="form-group">
+			<button on:click={clickSuper} class="btn">SUPER VIP</button>
+		</div>
+	</div>
+</section>
+
+<style lang="scss">
+	@import './static/style.scss';
+
+	.formulario {
+		margin: 120px 0px;
+		padding: 20px;
+
+		button {
+			width: 100%;
+		}
+	}
+	input {
+		width: 200px;
+		height: 40px;
+	}
+</style>
---
+++ src/routes/(manage)/check-in/[slug]/scanear/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+</script>
+
+<svelte:head>
+	<script src="https://cdn.jsdelivr.net/npm/dynamsoft-javascript-barcode/dist/dbr.js"></script>
+
+	<script>
+		// specify a license, you can visit https://www.dynamsoft.com/customer/license/trialLicense?utm_source=intro&product=dbr&package=js to get your own trial license good for 30 days.
+		Dynamsoft.DBR.BarcodeScanner.license = 'DLS2eyJoYW5kc2hha2VDb2RlIjoiMTAxODY1OTk3LVRYbFhaV0pRY205cVgyUmljZyIsIm1haW5TZXJ2ZXJVUkwiOiJodHRwczovL21sdHMuZHluYW1zb2Z0LmNvbSIsIm9yZ2FuaXphdGlvbklEIjoiMTAxODY1OTk3IiwiY2hlY2tDb2RlIjotMTQyOTM5NTc1N30=';
+		(async () => {
+			let scanner = await Dynamsoft.DBR.BarcodeScanner.createInstance();
+			scanner.onUniqueRead = (txt, result) => {
+				if (txt) {
+					if (txt.includes('quehay')) {
+						const ticketId = txt.substring(txt.lastIndexOf('/') + 1);
+						window.location.href = `/check-in/vilmapalma/qr/${ticketId}`;
+					} else {
+						let texto = txt.toString();
+						const ticketId = texto.substring(miString.length - 5);
+						if (texto.startsWith('11')) {
+							window.location.href = `/check-in/vilmapalma/impreso/SUPER/${ticketId}`;
+						} else if (texto.startsWith('12')) {
+							window.location.href = `/check-in/vilmapalma/impreso/VIP/${ticketId}`;
+						} else if (texto.startsWith('13')) {
+							window.location.href = `/check-in/vilmapalma/impreso/GENERAL/${ticketId}`;
+						}
+					}
+				}
+			};
+			await scanner.show();
+		})();
+	</script>
+</svelte:head>
