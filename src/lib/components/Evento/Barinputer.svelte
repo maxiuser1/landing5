5c246b58db6aa6267adbc3e3d1f54c8f557a247e
maxiuser1 <cloudencuestasinternas2021@hotmail.com>
2023-05-19 22:48:32 -0500

    intento 2

--- src/lib/components/Evento/Barinputer.svelte
+++ src/lib/components/Evento/Barinputer.svelte
@@ -17,8 +17,6 @@
 				dispatch('detected', { text: txt });
 			};
 
-			await scanner.updateRuntimeSettings('dense');
-			await scanner.setResolution(3840, 2160);
 			await scanner.open();
 		} catch (error) {
 			console.error(error);
