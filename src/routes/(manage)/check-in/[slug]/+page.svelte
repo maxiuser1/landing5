f9a26b9ad5a7b1b8ffc04e2b8f8573f62f9f7eeb
maxiuser1 <cloudencuestasinternas2021@hotmail.com>
2023-05-20 07:29:08 -0500

    arreglo urban

--- src/routes/(manage)/check-in/[slug]/+page.svelte
+++ src/routes/(manage)/check-in/[slug]/+page.svelte
@@ -5,8 +5,8 @@
 	<div class="seccion">
 		<h3>Picado de tickets</h3>
 		<div class="botones">
-			<a href="/check-in/vilmapalma/scanear" class="btn">Scanear</a>
-			<a href="/check-in/vilmapalma/manual" class="btn">Manual</a>
+			<a href="/check-in/urban/scanear" class="btn">Scanear</a>
+			<a href="/check-in/urban/manual" class="btn">Manual</a>
 		</div>
 	</div>
 </section>
