bee46fdce60ce361a480b73559ac206bc2f362c1
jcalderon <pe.jose.calderon@gmail.com>
2025-03-15 07:00:39 -0500

    login 1

--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
 	import { page } from '$app/state';
-	import { Google } from '$lib/icons';
+	import { Arrow, Google } from '$lib/icons';
 	import { GoogleAuthProvider, onAuthStateChanged, signInWithPopup } from 'firebase/auth';
 	import { auth } from '../../../firebase';
 	import { invalidateAll } from '$app/navigation';
 	import { applyAction } from '$app/forms';
 	import { onMount } from 'svelte';
 
+	let userError = $state('');
+	let passError = $state('');
 	onAuthStateChanged(auth, async (user) => {
 		if (user) {
 			const data = new FormData();
@@ -47,7 +49,47 @@
 	}
 </script>
 
-<div>
+<div class="login">
+	<form method="POST" action="?/login" class="mt-20">
+		<div class="form mt-20">
+			<h1 class="titulo--suprayado">Bienvenido</h1>
+			<div class="form-group">
+				<label for="username">Usuario</label>
+				<input
+					type="text"
+					name="username"
+					placeholder="Ejem. usuario@mail.com"
+					class:error={userError ? true : false}
+				/>
+				{#if userError}
+					<div class="error">{userError}</div>
+				{/if}
+			</div>
+			<div class="form-group">
+				<label for="password">Contraseña</label>
+				<input type="password" name="password" placeholder="*********" class:error={passError ? true : false} />
+				{#if passError}
+					<div class="error">{passError}</div>
+				{/if}
+
+				<div class="opciones">
+					<div class="checkboxes">
+						<div>
+							<input type="checkbox" name="recordar" />
+						</div>
+						<div>
+							<label for="recordar">Recordar usuario</label>
+						</div>
+					</div>
+					<a href="/recuperar" class="link">¿Olvidaste tu contraseña?</a>
+				</div>
+			</div>
+			<div>
+				<button class="btn mt-20" type="submit"><span>Ingresar</span>&nbsp; <Arrow /></button>
+			</div>
+		</div>
+	</form>
+
 	<div class="socials">
 		<form method="POST" onsubmit={handleGoogleClick}>
 			<button class="btn-social" type="submit"> <Google /> </button>
@@ -56,6 +98,40 @@
 </div>
 
 <style lang="scss">
+	@use '$lib/scss/form' as form;
+
+	.checkboxes {
+		display: flex;
+		justify-content: start;
+		align-items: center;
+		gap: 8px;
+
+		label {
+			font-size: 14px !important;
+			font-weight: 300 !important;
+		}
+
+		input {
+			width: 20px;
+			height: 20px;
+		}
+	}
+	.opciones {
+		display: flex;
+		justify-content: space-between;
+
+		.link {
+			margin-top: 12px;
+			color: #d30ed1;
+		}
+	}
+
+	.login {
+		margin: 0 auto;
+		max-width: 95wh;
+		padding-top: 12px;
+	}
+
 	.socials {
 		margin-top: 24px;
 		margin-bottom: 24px;
